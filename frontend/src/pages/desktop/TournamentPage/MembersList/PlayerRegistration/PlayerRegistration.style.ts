1cc3df0729d3dda8d658c478bd55df1eabc6a12e
Landry Trebon <landry.trebon@gmail.com>
2024-06-28 22:27:10 +0200

    members list

--- frontend/src/pages/desktop/TournamentPage/MembersList/PlayerRegistration/PlayerRegistration.style.ts
+++
@@ -1,6 +0,0 @@
-const PlayerRegistrationStyle = {
-  base: "flex text-black gap-2 items-center",
-  name: "flex-1",
-};
-
-export default PlayerRegistrationStyle;
--- frontend/src/pages/desktop/TournamentPage/MembersList/PlayerRegistration/PlayerRegistration.tsx
+++ frontend/src/pages/desktop/TournamentPage/MembersList/PlayerRegistration/PlayerRegistration.tsx
@@ -2,24 +2,31 @@ import {
   faCheck,
   faHourglass,
 } from "@fortawesome/free-solid-svg-icons";
+import { twMerge } from "tailwind-merge";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Tag from "../../../../../components/Tag/Tag";
 import { Registration } from "../../../../../utils/registration";
-import PlayerRegistrationStyle from "./PlayerRegistration.style";
 
 type Props = {
   registration: Registration;
+  style?: string;
 };
 
-export default function PlayerRegistration({ registration }: Props) {
+export default function PlayerRegistration({
+  registration,
+  style,
+}: Props) {
   const partnerStr = registration.partner
     ? `Avec ${registration.partner.lastname} ${registration.partner.name} | `
     : "";
 
   return (
     <div
-      className={PlayerRegistrationStyle.base}
+      className={twMerge(
+        "flex text-black gap-4 items-center p-2 px-4 rounded-2xl",
+        style,
+      )}
       title={
         registration.sent
           ? `${partnerStr}Inscription envoyée`
@@ -28,12 +35,19 @@ export default function PlayerRegistration({ registration }: Props) {
     >
       <FontAwesomeIcon
         size="sm"
-        color={registration.sent ? "#80ed99" : undefined}
         icon={registration.sent ? faCheck : faHourglass}
       />
-      <p className={PlayerRegistrationStyle.name}>
-        {registration.player.lastname} {registration.player.name}
-      </p>
+      <div className="flex-1">
+        <p>
+          {registration.player.lastname} {registration.player.name}
+        </p>
+        {registration.partner && (
+          <p>
+            {registration.partner.lastname}{" "}
+            {registration.partner.name}
+          </p>
+        )}
+      </div>
       <Tag size="sm">{registration.discipline}</Tag>
     </div>
   );
