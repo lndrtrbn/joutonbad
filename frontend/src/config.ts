ce7892c20c8ccfa18ebbdc747dcf86ade1adebe6
Landry Trebon <landry.trebon@gmail.com>
2024-10-10 19:38:23 +0200

    some updates

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
   joutonbad: {
     apiUrl: import.meta.env.VITE_API_URL,
     clientName: import.meta.env.VITE_JOUTONBAD_CLIENT,
-    moderatorRole: import.meta.env.VITE_MODERATOR_ROLE,
+    moderatorRole: import.meta.env.VITE_AUTH0_MODERATOR_ROLE,
   },
   auth0: {
     clientDomain: import.meta.env.VITE_AUTH0_CLIENT_DOMAIN,
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
-  readonly VITE_MODERATOR_ROLE: string;
+  readonly VITE_AUTH0_MODERATOR_ROLE: string;
   readonly VITE_JOUTONBAD_CLIENT: string;
   readonly VITE_AUTH0_CLIENT_DOMAIN: string;
   readonly VITE_AUTH0_CLIENT_ID: string;
